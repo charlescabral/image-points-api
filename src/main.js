4168b7dfcba897d0860afd2296e11e67da00f3b5
Charles Cabral <charles.cabral.neto@gmail.com>
2018-05-29 13:09:06 -0300

    new version

--- src/main.js
+++ src/main.js
@@ -1,85 +1,180 @@
-$(function() {
+"use strict";
 
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext("2d");
-    
-    // Map sprite
-    var mapSprite = new Image();
-    mapSprite.src = "https://img.elo7.com.br/product/main/1486F1F/difusor-de-ambiente-1-litro-cheiro.jpg";
-    
-    var Marker = function () {
-        this.Sprite = new Image();
-        this.Sprite.src = "http://www.clker.com/cliparts/w/O/e/P/x/i/map-marker-hi.png"
-        this.Width = 12;
-        this.Height = 20;
-        this.XPos = 0;
-        this.YPos = 0;
-    }
-    
-    var Markers = new Array();
-    
-    var mouseClicked = function (mouse) {
-        // Get corrent mouse coords
-        var rect = canvas.getBoundingClientRect();
-        var mouseXPos = (mouse.x - rect.left);
-        var mouseYPos = (mouse.y - rect.top);
-    
-        console.log("Marker added");
-    
-        // Move the marker when placed to a better location
-        var marker = new Marker();
-        marker.XPos = mouseXPos - (marker.Width / 2);
-        marker.YPos = mouseYPos - marker.Height;
-    
-        Markers.push(marker);
-    }
-    
-    // Add mouse click event listener to canvas
-    canvas.addEventListener("mousedown", mouseClicked, false);
-    
-    var firstLoad = function () {
-        context.font = "15px Georgia";
-        context.textAlign = "center";
-    }
-    
-    firstLoad();
-    
-    var main = function () {
-        draw();
+(function ($) {
+
+    // returns whether values are less than (maxDev * 100)% away
+    var isNearby = function (one, two, maxDev) {
+        return Math.abs(1 - Math.abs(one / two)) < maxDev;
     };
-    
-    var draw = function () {
-        // Clear Canvas
-        context.fillStyle = "#000";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-    
-        // Draw map
-        // Sprite, X location, Y location, Image width, Image height
-        // You can leave the image height and width off, if you do it will draw the image at default size
-        context.drawImage(mapSprite, 0, 0, 580, 392);
-    
-        // Draw markers
-        for (var i = 0; i < Markers.length; i++) {
-            var tempMarker = Markers[i];
-            // Draw marker
-            context.drawImage(tempMarker.Sprite, tempMarker.XPos, tempMarker.YPos, tempMarker.Width, tempMarker.Height);
-    
-            // Calculate postion text
-            var markerText = "Postion (X:" + tempMarker.XPos + ", Y:" + tempMarker.YPos;
-    
-            // Draw a simple box so you can see the position
-            var textMeasurements = context.measureText(markerText);
-            context.fillStyle = "#666";
-            context.globalAlpha = 0.7;
-            context.fillRect(tempMarker.XPos - (textMeasurements.width / 2), tempMarker.YPos - 15, textMeasurements.width, 20);
-            context.globalAlpha = 1;
-    
-            // Draw position above
-            context.fillStyle = "#000";
-            context.fillText(markerText, tempMarker.XPos, tempMarker.YPos);
+
+    // dot helper class
+    // x,y are assumend to already be relative to align unless el is not undefined
+    var dot = function (_x, _y, _text, _align, el) {
+        this.x = _x;
+        this.y = _y;
+        this.text = _text; // dot text
+        this.align = _align; // {x, y} to which these coords are relative to
+        this.radius = 24; // hover event radius
+
+        if(el != undefined)
+        {
+            this.x = (this.x * this.align.x) / $(el).width();
+            this.y = (this.y * this.align.y) / $(el).height();
         }
-    };
-    
-    setInterval(main, (1000 / 60)); // Refresh 60 times a second
-});
 
+        this.relativeX = function (el) {
+            return (this.x * $(el).width()) / this.align.x;
+        };
+
+        this.relativeY = function (el) {
+            return (this.y * $(el).height()) / this.align.y;
+        };
+
+        this.render = function (context, el) {
+            context.beginPath();
+            context.arc(this.relativeX(el), this.relativeY(el), 4, 0, Math.PI * 2, true);
+            context.fill();
+        };
+
+        // returns whether (x,y) match this point when relative to el or, if
+        // el == undefined, they're matching (== diffrence less than maxDeviration)
+        this.isMe = function (x, y, el, maxDeviration) {
+            maxDeviration = maxDeviration || 0.01;
+            if(el != undefined)
+            {
+                x = (x * this.align.x) / $(el).width();
+                y = (y * this.align.y) / $(el).height();
+            }
+            return isNearby(x, this.x, maxDeviration) && isNearby(y, this.y, maxDeviration);
+        };
+    };
+
+    $.fn.dots = function (dots, options) {
+        var settings = $.extend({
+            img: "",
+            setmode: false,
+            setcallback: function (dot) {},
+            defaulttext: "New Item",
+            forceRatio: false,
+            align: {
+                x: 770,
+                y: 566
+            }
+        }, options );
+
+        // initialize dots
+        settings.dots = [];
+        $.each(dots, function (i, el) {
+            settings.dots.push(new dot(el.x, el.y, el.text, settings.align));
+        });
+
+        // create tooltip canvas
+        var tooltip = $('<div/>')
+            .appendTo('body')
+            .addClass('hoverDotTooltip')
+            .css({
+                backgroundColor: 'white',
+                border: '1px solid transparent',
+                position: 'absolute',
+                padding: '5px',
+                marginLeft: '-50px'
+            })
+            .hide();
+
+        var contexts = [];
+
+        var showHideTooltip = function (show) {
+            if(show) {
+                tooltip.show();
+                tooltip.get(0).style.marginLeft = ((-tooltip.width() / 2) + 3) + "px";
+            } else {
+                tooltip.hide();
+            }
+        };
+
+        // handle tooltip positioning on mouse move
+        var mouseMoveEvent = function(event, img, offsetX, offsetY) {
+            var mouseX = parseInt(event.clientX - offsetX + $(window).scrollLeft());
+            var mouseY = parseInt(event.clientY - offsetY + $(window).scrollTop());
+
+            var hit = false;
+            for (var i = 0; i < settings.dots.length; i++) {
+                var dot = settings.dots[i];
+                var dx = mouseX - dot.relativeX(img);
+                var dy = mouseY - dot.relativeY(img);
+
+                if (dx * dx + dy * dy < dot.radius) {
+                    tooltip.get(0).style.top = ((dot.relativeY(img) - 40) + offsetY) + "px"; // -40 to "center" the canvas
+                    tooltip.get(0).style.left = (dot.relativeX(img) + offsetX) + "px";
+
+                    tooltip.html(dot.text);
+                    hit = true;
+                }
+            }
+
+            showHideTooltip(hit);
+        };
+
+        // re-renders all dots
+        var render = function () {
+            $.each(contexts, function (i, data) {
+                $.each(settings.dots, function (j, dot) {
+                    dot.render(data.ctx, data.el);
+                });
+            });
+        };
+
+        // places a new dot
+        var placedot = function (event, element) {
+            var ndot = new dot(event.clientX - element.offsetLeft,
+                event.clientY - element.offsetTop + $(window).scrollTop(),
+                settings.defaulttext,
+                settings.align,
+                element);
+
+            settings.dots.push(ndot);
+            render();
+            settings.setcallback(ndot);
+        };
+
+        // removes a dot
+        this.removeDot = function(x, y) {
+            settings.dots = settings.dots.filter(function(el, index) {
+                return dot.x != x && dot.y != y;
+            });
+        };
+
+        // init mouse move on each element
+        this.each(function(i, el) {
+            el.style= 'background: url(' + settings.img + ');' +
+            'background-repeat: no-repeat; ' +
+            'background-size: 100% 100%;' +
+            'width: ' + settings.width + '; ';
+            //'height: ' + settings.height + '; '
+
+            var jqel = $(el);
+
+            contexts.push( { el: el, ctx: el.getContext('2d') });
+
+            jqel.mousemove (function (event) {
+                mouseMoveEvent(event, el, el.offsetLeft, el.offsetTop);
+            });
+
+            if(settings.setmode) jqel.click(function (e) {
+                placedot(e, el);
+            });
+
+            jqel.resize(function (e) {
+                if(isNumeric(settings.forceRatio))
+                    jqel.height(jqel.width() * settings.forceRatio);
+                render();
+            });
+        });
+
+        // render initial dots
+        render();
+
+        return this;
+    };
+
+})(jQuery);
